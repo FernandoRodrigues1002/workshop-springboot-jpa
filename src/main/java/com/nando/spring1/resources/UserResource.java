265de5dba0d5d54c3ba625a2c9d19bfe257dfd2b
Nando <fernanrods1002@gmail.com>
2025-07-22 19:49:06 -0300

    service e component registration

--- src/main/java/com/nando/spring1/resources/UserResource.java
+++ src/main/java/com/nando/spring1/resources/UserResource.java
@@ -1,20 +1,38 @@
 package com.nando.spring1.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.nando.spring1.entities.User;
+import com.nando.spring1.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User( "nando", "nando@mail", "9999999", "12345");
-        return ResponseEntity.ok().body(u);
+    @Autowired
+    private UserService service;
+
+    @GetMapping // retornando umma lista de usuarios
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
+    
+
+
 
 }
